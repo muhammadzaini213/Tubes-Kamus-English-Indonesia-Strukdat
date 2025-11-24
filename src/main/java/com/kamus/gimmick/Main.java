6f29d8cdd5a7a71b6d7b3402535ed02d030a8af7
Primaphoe24 <11241074@student.itk.ac.id>
2025-11-24 08:53:33 +0800

    Sayang

---
+++ src/main/java/com/kamus/gimmick/Main.java
@@ -0,0 +1,24 @@
+package com.kamus.gimmick;
+
+import javafx.application.Application;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("./fxml/main.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setTitle("Kamus JavaFX");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
